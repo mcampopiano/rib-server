ab8cca7374d4c49034a34055882906a980b27b93
Mario Campopiano <mcampopiano92@gmail.com>
2021-04-12 19:29:07 -0500

    made fixtures for tokens, users, contractors, clients

--- ribapi/models/contractor.py
+++ ribapi/models/contractor.py
@@ -2,5 +2,4 @@ from django.db import models
 from rest_framework.authtoken.models import Token
 
 class Contractor(models.Model):
-    user = models.ForeignKey(Token, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
