4f39b2f7db83730771cef1394dcd38309d4ec5bb
Mario Campopiano <mcampopiano92@gmail.com>
2021-04-20 15:33:13 -0500

    added calculation for wall damage

--- ribapi/views/rooms.py
+++ ribapi/views/rooms.py
@@ -21,18 +21,31 @@ class Rooms(ViewSet):
     def create(self, request):
         room = Room()
         sq = request.data['length'] * request.data['width']
+        cf = sq * request.data['height']
         air_movers = 1
 
         room.name = request.data['name']
         room.width = request.data['width']
         room.length = request.data['length']
+        room.height = request.data['height']
         room.air_movers_min = air_movers + math.ceil(sq/70)
         room.air_movers_max = air_movers + math.ceil(sq/50)
+
+        # Use if damage above two feet on the walls
+        if len(request.data['walls']) > 0:
+            sq_walls = 0
+            for wall in request.data['walls']:
+                height = wall['height'] - 2
+                sq_walls = sq_walls + (height * wall['length'])
+            room.air_movers_min = room.air_movers_min + math.ceil(sq_walls/150)
+            room.air_movers_max = room.air_movers_min + math.ceil(sq_walls/100)
+            print(sq_walls)
 
         room.dehumidifier_min_size = None
         room.dehumidifier_max_size = None
         room.ceiling_damage = False
         room.damage_above_two_feet = False
+        # room.save()
 
         serializer = RoomSerializer(room, many=False, context={'request': request})
         return Response(serializer.data, status=status.HTTP_201_CREATED)
